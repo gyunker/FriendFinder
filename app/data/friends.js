b4939d435a6725469d9207c6ea729defeef5a6c4
Grant Yunker <grantyunker@gmail.com>
2018-08-16 21:04:30 -0700

    beta

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// ===============================================================================
+// FRIEND DATA
+// ===============================================================================
+
+var friendsArray = [
+    {
+        "name":"Peter",
+        "photo":"https://static.boredpanda.com/blog/wp-content/uploads/2017/09/funny-dog-thoughts-tweets-1.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+];
+
+
+module.exports = friendsArray; 
